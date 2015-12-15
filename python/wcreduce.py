328b21426200622ad7a1c2c541aa601dec3be7ad
Stefaan Lippens <soxofaan@gmail.com>
2015-12-15 12:06:19 +0100

    whitespace cleanup

--- python/wcreduce.py
+++ python/wcreduce.py
@@ -4,28 +4,28 @@ import sys
 from wcmap import output_key_value
 
 def read(input=sys.stdin):
-	for line in input:
-		try:
-			word, count = line.split('\t', 1)
-			yield word, int(count)
-		except StandardError:
-			print >>sys.stderr, 'Ignoring', repr(line)
+    for line in input:
+        try:
+            word, count = line.split('\t', 1)
+            yield word, int(count)
+        except StandardError:
+            print >>sys.stderr, 'Ignoring', repr(line)
 
 
 def main(input=sys.stdin):
-	prev_word = None
-	total = 0
-	for word, count in read(input=input):
-		if word == prev_word:
-			total += count
-		else:
-			if prev_word is not None:
-				output_key_value(prev_word, total)
-			prev_word = word
-			total = count
+    prev_word = None
+    total = 0
+    for word, count in read(input=input):
+        if word == prev_word:
+            total += count
+        else:
+            if prev_word is not None:
+                output_key_value(prev_word, total)
+            prev_word = word
+            total = count
 
-	output_key_value(prev_word, total)
+    output_key_value(prev_word, total)
 
 
 if __name__ == '__main__':
-	main()
+    main()
